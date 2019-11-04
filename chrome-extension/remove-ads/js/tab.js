e04564df2d917c14833e3dde487bccd6dfdf723a
mrlhz <mr__lhz@163.com>
2019-11-05 06:38:15 +0800

    add newtab

---
+++ chrome-extension/remove-ads/js/tab.js
@@ -0,0 +1,270 @@
+const data = [{
+    "url": "http://www.w3schools.com/",
+    "img": "http://chuangzaoshi.com/assets/images/C/w3schools.png",
+    "title": "w3schools",
+    "desc": "WEB初学者教程网站",
+    "type": "［前端］热门推荐"
+  },
+  {
+    "url": "http://getbootstrap.com/",
+    "img": "http://chuangzaoshi.com/assets/images/C/bootstrap.png",
+    "title": "Bootstrap",
+    "desc": "用于前端开发的开源工具包",
+    "type": "［前端］热门推荐"
+  },
+  {
+    "url": "http://codepen.io/",
+    "img": "http://chuangzaoshi.com/assets/images/C/codepen.png",
+    "title": "codepen",
+    "desc": "前端炫酷样式技能效果",
+    "type": "［前端］热门推荐"
+  },
+  {
+    "url": "http://fontawesome.io/",
+    "img": "http://chuangzaoshi.com/assets/images/C/fontawesome.png",
+    "title": "Fontawesome",
+    "desc": "全球著名的前端图标字体库",
+    "type": "［前端］热门推荐"
+  },
+  {
+    "url": "http://oops.re/",
+    "img": "http://chuangzaoshi.com/assets/images/C/oops.png",
+    "title": "OOPS",
+    "desc": "404优秀设计案例展示",
+    "type": "［前端］热门推荐"
+  },
+  {
+    "url": "https://getmimo.com/",
+    "img": "http://chuangzaoshi.com/assets/images/C/mimo.png",
+    "title": "Mimo APP",
+    "desc": "一款很棒的编程学习APP",
+    "type": "［前端］热门推荐"
+  },
+  {
+    "url": "http://codecast.me/",
+    "img": "http://chuangzaoshi.com/assets/images/C/codecast.png",
+    "title": "CodeCast",
+    "desc": "在线实时编程预览",
+    "type": "［前端］热门推荐"
+  },
+  {
+    "url": "http://navnav.co/",
+    "img": "http://chuangzaoshi.com/assets/images/C/navnav.png",
+    "title": "Navnav",
+    "desc": "CSS/JS炫酷前端动效",
+    "type": "［前端］热门推荐"
+  },
+  {
+    "url": "https://weui.io/",
+    "img": "http://chuangzaoshi.com/assets/images/C/weui.png",
+    "title": "WEUI",
+    "desc": "微信官方小程序UI样式库",
+    "type": "［前端］热门推荐"
+  },
+  {
+    "url": "https://code.facebook.com/projects/",
+    "img": "http://chuangzaoshi.com/assets/images/C/facebookcode.png",
+    "title": "Facebook-code",
+    "desc": "facebook开放资源",
+    "type": "［前端］热门推荐"
+  },
+  {
+    "url": "http://snapsvg.io/",
+    "img": "http://chuangzaoshi.com/assets/images/C/snapsvg.png",
+    "title": "SnapSVG",
+    "desc": "SVG动效的JS库",
+    "type": "［前端］热门推荐"
+  },
+  {
+    "url": "https://jsfiddle.net/",
+    "img": "http://chuangzaoshi.com/assets/images/C/jsfiddle.png",
+    "title": "JSfiddle",
+    "desc": "在线实时JS测试编程",
+    "type": "［前端］热门推荐"
+  },
+  {
+    "url": "https://codemyui.com/",
+    "img": "http://chuangzaoshi.com/assets/images/C/codemyui.png",
+    "title": "codemyui",
+    "desc": "采集有趣的网站UI代码片段",
+    "type": "［前端］热门推荐"
+  },
+  {
+    "url": "https://tympanus.net/codrops/",
+    "img": "http://chuangzaoshi.com/assets/images/C/codrops.png",
+    "title": "codrops",
+    "desc": "网站UI交互源码分享",
+    "type": "［前端］热门推荐"
+  },
+  {
+    "url": "http://htmlarrows.com/",
+    "img": "http://chuangzaoshi.com/assets/images/C/htmlarrows.png",
+    "title": "htmlarrows",
+    "desc": "提供HMTL的特殊符号",
+    "type": "［前端］热门推荐"
+  },
+  {
+    "url": "http://www.copypastecharacter.com/",
+    "img": "http://chuangzaoshi.com/assets/images/C/copypastecharacter.png",
+    "title": "copypastecharacter",
+    "desc": "特殊符号大全表",
+    "type": "［前端］热门推荐"
+  },
+  {
+    "url": "http://cssdeck.com/",
+    "img": "http://chuangzaoshi.com/assets/images/C/cssdeck.png",
+    "title": "CSSDeck",
+    "desc": "在线CSS前端代码Demo样式效果",
+    "type": "［前端］热门推荐"
+  },
+  {
+    "url": "http://animista.net/",
+    "img": "http://chuangzaoshi.com/assets/images/C/animista.png",
+    "title": "Animista",
+    "desc": "CSS动态效果样式在线自定义",
+    "type": "［前端］热门推荐"
+  },
+  {
+    "url": "https://platform.html5.org/",
+    "img": "http://chuangzaoshi.com/assets/images/C/platform.png",
+    "title": "html5 Platform",
+    "desc": "HTML5的web浏览技术图谱",
+    "type": "［前端］热门推荐"
+  },
+  {
+    "url": "http://chuangzaoshi.com/icon/",
+    "img": "http://chuangzaoshi.com/assets/images/D/caomei.png",
+    "title": "草莓图标",
+    "desc": "为开发者设计的一套免费开源图标库",
+    "type": "［前端］热门推荐"
+  },
+  {
+    "url": "https://www.raspberrypi.org/",
+    "img": "http://chuangzaoshi.com/assets/images/C/raspberry.png",
+    "title": "树莓派",
+    "desc": "编程教育而设计的微电脑",
+    "type": "［前端］热门推荐"
+  },
+  {
+    "url": "https://w3techs.com",
+    "img": "http://chuangzaoshi.com/assets/images/C/w3techs.png",
+    "title": "w3techs",
+    "desc": "web网站技术调查报告",
+    "type": "［前端］热门推荐"
+  },
+  {
+    "url": "https://css-tricks.com/",
+    "img": "http://chuangzaoshi.com/assets/images/C/css-tricks.png",
+    "title": "CSS-Tricks",
+    "desc": "优秀的CSS前端技术分享博客",
+    "type": "［前端］热门推荐"
+  },
+  {
+    "url": "https://threejs.org/",
+    "img": "",
+    "title": "three.js",
+    "desc": "JavaScript编写的WebGL第三方3D库",
+    "type": "［前端］热门推荐"
+  },
+  {
+    "url": "https://facebook.github.io/react/",
+    "img": "http://chuangzaoshi.com/assets/images/C/reactjs.png",
+    "title": "React",
+    "desc": "facebook强大的JavaScript框架",
+    "type": "［前端］前端框架"
+  },
+  {
+    "url": "https://angularjs.org/",
+    "img": "http://chuangzaoshi.com/assets/images/C/angularjs.png",
+    "title": "Angularjs",
+    "desc": "Google开发的前端技术框架",
+    "type": "［前端］前端框架"
+  },
+  {
+    "url": "http://jquery.com/",
+    "img": "http://chuangzaoshi.com/assets/images/C/jquery.png",
+    "title": "JQuery",
+    "desc": "一个快速、简洁的JavaScript框架",
+    "type": "［前端］前端框架"
+  },
+  {
+    "url": "http://foundation.zurb.com/",
+    "img": "http://chuangzaoshi.com/assets/images/C/foundation.png",
+    "title": "Foundation",
+    "desc": "高级响应式前端框架",
+    "type": "［前端］前端框架"
+  },
+  {
+    "url": "http://www.material-ui.com/",
+    "img": "http://chuangzaoshi.com/assets/images/C/material-ui.png",
+    "title": "Material UI",
+    "desc": "一组反应元件，实现谷歌的材料设计",
+    "type": "［前端］前端框架"
+  },
+  {
+    "url": "http://cn.vuejs.org/",
+    "img": "http://chuangzaoshi.com/assets/images/C/vuejs.png",
+    "title": "Vuejs",
+    "desc": "小巧精致,渐进式JavaScript 框架",
+    "type": "［前端］前端框架"
+  },
+  {
+    "url": "http://sass-lang.com/",
+    "img": "http://chuangzaoshi.com/assets/images/C/sass.png",
+    "title": "Sass",
+    "desc": "世界级强大的专业CSS扩展语言",
+    "type": "［前端］前端框架"
+  },
+  {
+    "url": "https://getmdl.io/",
+    "img": "http://chuangzaoshi.com/assets/images/C/getmdl.png",
+    "title": "MaterialDesignLite",
+    "desc": "Google官方轻量级MaterialDesignLite版",
+    "type": "［前端］前端框架"
+  },
+  {
+    "url": "https://material.io/guidelines/",
+    "img": "http://chuangzaoshi.com/assets/images/C/material.png",
+    "title": "Material Design",
+    "desc": "Google官方MaterialDesign指南",
+    "type": "［前端］前端框架"
+  },
+  {
+    "url": "http://designmodo.github.io/Flat-UI/",
+    "img": "http://chuangzaoshi.com/assets/images/C/flatui.png",
+    "title": "Flat UI",
+    "desc": "扁平化多彩风格UI",
+    "type": "［前端］前端框架"
+  },
+  {
+    "url": "https://purecss.io/",
+    "img": "http://chuangzaoshi.com/assets/images/C/purecss.png",
+    "title": "Pure",
+    "desc": "轻型响应式CSS框架",
+    "type": "［前端］前端框架"
+  },
+  {
+    "url": "https://semantic-ui.com/",
+    "img": "http://chuangzaoshi.com/assets/images/C/semanticui.png",
+    "title": "Semantic UI",
+    "desc": "语义化设计的前端开源框架",
+    "type": "［前端］前端框架"
+  }
+]
+
+
+const str = data.map((item) => {
+  return `
+  <a href="${item.url}" class="item">
+    <div class="logo">
+      <div class="el-image">
+        <img src="${item.img}" alt="${item.title}">
+      </div>
+      <span>${item.title}</span>
+    </div>
+    <div class="desc">${item.desc}</div>
+  </a>
+  `
+}).join('');
+
+document.querySelector('.app').innerHTML = str
