97f9417f3ccaa78c88e2cc92c0cfd574a6e8d0b2
mrlhz <mr__lhz@163.com>
2021-11-27 00:38:58 +0800

    download video

--- chrome-extension/study/background.js
+++ chrome-extension/study/background.js
@@ -12,6 +12,7 @@ chrome.runtime.onInstalled.addListener(function () {
   })
 })
 
+
 chrome.runtime.onInstalled.addListener(function () {
   chrome.contextMenus.create({
     'id': 'newTabs',
@@ -20,18 +21,65 @@ chrome.runtime.onInstalled.addListener(function () {
   })
 })
 
+chrome.runtime.onInstalled.addListener(function () {
+  chrome.contextMenus.create({
+    'id': 'downloadAll',
+    'type': 'normal',
+    'title': 'download all',
+  })
+})
 
-chrome.contextMenus.onClicked.addListener(function (info, tab) {
+
+chrome.contextMenus.onClicked.addListener(async function (info, tab) {
   console.log(info, tab)
   if (info.menuItemId == 'downloadVideo') {
     // chrome.scripting.executeScript({target: { tabId: tab.id }, func: getHtml }, onCall)
     // todo 应该先执行getHdLink， 再执行getHtml
-    chrome.scripting.executeScript({ target: { tabId: tab.id }, func: getHtml, args: ['task'] }, onCall)
+    // chrome.scripting.executeScript({ target: { tabId: tab.id }, func: getHtml, args: ['task'] }, onCall)
+    const tab = await getCurrentTab()
+    const h = await executeScript(tab, getHdLink)
+    console.log(h, tab)
+    await wait(1200)
+    const res = await executeScript(tab, getHtml)
+    console.log(res)
+    await onDownload(res)
   } else if (info.menuItemId == 'newTabs') {
-    chrome.scripting.executeScript({ target: { tabId: tab.id }, func: getWellList, args: ['task'] }, onWellCall)
-    
+    // chrome.scripting.executeScript({ target: { tabId: tab.id }, func: getWellList, args: ['task'] }, onWellCall)
+    const [{ frameId, result }] = await executeScript(tab, getWellList)
+    console.log(result)
+    const task = result.map(item => chrome.tabs.create({ url: item.href }))
+    const response = await Promise.all(task)
+    // const hd = await Promise.all(response.map(item => executeScript(item, getHdLink)))
+    console.log(response)
+  } else if (info.menuItemId === 'downloadAll') {
+    const tabs = await getAllWindow()
+    console.log('tabs', tabs)
+    // filter
+    const targetTabs = tabs.filter(tab => tab.url && tab.url.includes('view_video'))
+    console.log(targetTabs)
+    const tabTask = targetTabs.map(tab => executeScript(tab, getHdLink))
+    await Promise.all(tabTask)
+    await wait(1200)
+    console.log('init')
+    const downloadInfoTask = targetTabs.map(tab => executeScript(tab, getHtml))
+    const downloadInfo = await Promise.all(downloadInfoTask)
+    console.log('info', downloadInfo)
+    const downloadTask = downloadInfo.map(item => onDownload(item))
+    const res = await Promise.all(downloadTask)
   }
 })
+
+async function onDownload(results = []) {
+  const _result = results[0]
+  if (!_result || !_result.result.downloadLink) {
+    console.log('no file', _result)
+    return
+  }
+  const { result, frameId } = _result
+  const { downloadLink, title, time, author } = result
+  const res = await chrome.downloads.download({ url: downloadLink, filename: `91/[${author}]-${title}-${time}.mp4` })
+  console.log(res)
+}
 
 async function onCall(results = []) {
   const tab = await getCurrentTab()
@@ -70,9 +118,30 @@ async function onWellCall([ { result } ] = []) {
 
 
 async function getCurrentTab() {
-  let queryOptions = { active: true, currentWindow: true };
-  let [tab] = await chrome.tabs.query(queryOptions);
-  return tab;
+  let queryOptions = { active: true, currentWindow: true }
+  let [tab] = await chrome.tabs.query(queryOptions)
+  return tab
+}
+
+async function getAllWindow() {
+  let queryOptions = {}
+  let tabs = await chrome.tabs.query(queryOptions)
+  return tabs
 }
 // 图标和图形生成器
 // https://zh-cn.cooltext.com/
+
+
+function onTabUrlUpdated(tabId) {
+  return new Promise((resolve, reject) => {
+    const onUpdated = (id, info) => id === tabId && info.url && done(true)
+    const onRemoved = id => id === tabId && done(false)
+    chrome.tabs.onUpdated.addListener(onUpdated)
+    chrome.tabs.onRemoved.addListener(onRemoved)
+    function done(ok) {
+      chrome.tabs.onUpdated.removeListener(onUpdated)
+      chrome.tabs.onRemoved.removeListener(onRemoved)
+      (ok ? resolve : reject)()
+    }
+  })
+}
