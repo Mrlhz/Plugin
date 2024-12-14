3235f1f86ddb42bf4c7c9dd5e90e0c0ff0b82028
Mrlhz <mr__lhz@163.com>
2024-12-14 18:51:13 +0800

    增加返回顶部按钮

--- chrome-extension/topic/js/content-script.js
+++ chrome-extension/topic/js/content-script.js
@@ -46,3 +46,30 @@ function parseQuery(search = '') {
   }
   return query
 }
+
+
+const scrollToTop = (element) => element.scrollIntoView({ behavior: 'smooth', block: 'start' })
+
+// 滚动到底部
+const scrollToBottom = (element) => element.scrollIntoView({ behavior: 'smooth', block: 'end' })
+// document.body.scrollIntoView({ behavior: 'smooth', block: 'start' })
+// document.querySelector('body').scrollIntoView({ behavior: 'smooth', block: 'start' })
+
+const backtopHtml = `
+  <i class="el-icon el-backtop__icon">
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1024 1024"><path fill="currentColor" d="M512 320 192 704h639.936z"></path></svg>
+  </i>
+`
+
+if (!document.getElementById('el_backtop')) {
+  const backtop = document.createElement('div');
+  backtop.innerHTML = backtopHtml;
+  backtop.id = 'el_backtop'
+  backtop.className = 'el-backtop'
+  // backtop.style= 'right: 20px; bottom: 20px;'
+  document.body.appendChild(backtop);
+  backtop.addEventListener('click', function(el) {
+    console.log(el.target?.getBoundingClientRect())
+    scrollToTop(document.body)
+  }, false)
+}
