7c674eb0baef7f6bd508b54dd5be3fcbf8367f5e
Mrlhz <mr__lhz@163.com>
2026-05-24 22:45:39 +0800

    优化获取html

--- chrome-extension/topic/js/dom.js
+++ chrome-extension/topic/js/dom.js
@@ -8,11 +8,11 @@ export function getTopicDetail(options) {
 
   let topicDom = '';
   if (allPage) {
-    topicDom = document.getElementById('wrap')
+    topicDom = document.getElementById('wrap')?.cloneNode(true);
   } else {
-    topicDom = document.querySelector('.t_msgfontfix')
-      || document.querySelector('.postmessage.firstpost')
-      || document.querySelector('.defaultpost');
+    topicDom = document.querySelector('.t_msgfontfix')?.cloneNode(true)
+      || document.querySelector('.postmessage.firstpost')?.cloneNode(true)
+      || document.querySelector('.defaultpost')?.cloneNode(true);
   }
   
   // 无权限
@@ -31,13 +31,20 @@ export function getTopicDetail(options) {
 
   const images = [...topicDom?.querySelectorAll('img')]
     .map(image => {
-      return image.getAttribute('src')
+      const src = image.getAttribute('src');
+      if (src.endsWith('none.gif') && image.getAttribute('file')) {
+        return image.getAttribute('file');
+      }
+      return src;
     })
     .filter(src => src.startsWith('http'));
+  
+  const topic = allPage ? document.querySelector("#nav").outerHTML + topicDom?.outerHTML : topicDom?.outerHTML;
+  // console.log('topic', topic);
 
   return {
     title,
-    topic: topicDom?.outerHTML,
+    topic,
     author,
     authorLink,
     images,
@@ -45,5 +52,18 @@ export function getTopicDetail(options) {
     tid,
     page
   }
+}
 
+export function getSearchpost() {
+  const link = document.querySelector("#profile_act > li.searchpost > a");
+  const name = document.querySelector("#profilecontent > div.itemtitle.s_clear > h1");
+
+  const href = link?.getAttribute('href') || '';
+  const url = href.startsWith('http') ? href : `${window.location.origin}/${href}`
+
+  return {
+    name: name.innerText || '',
+    original: url,
+    url: href.startsWith('/') ? href : `/${href}`
+  }
 }
